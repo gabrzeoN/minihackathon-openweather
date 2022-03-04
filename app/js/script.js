66116a5f8cb00abe02a6439e0051c0e8250a115a
Gabriel Cari <g.cari@hotmail.com>
2022-03-04 19:03:41 -0300

    pull commit

--- app/js/script.js
+++ app/js/script.js
@@ -2,18 +2,19 @@ let latitude = null;
 let longitude = null;
 
 function callApiWeather() {
-  const promise = axios
-    .get("https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&units=metric&appid=80a839bb2d8a77c600954c9c171bd4a5");
+    const promise = axios
+        .get("https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&units=metric&appid=80a839bb2d8a77c600954c9c171bd4a5");
 
-  promise.then((response) => createHTML(response));
+    promise.then((response) => createHTML(response));
 }
 
 function createHTML(response) {
-  const wheatherInformation = document.querySelector(".weather");
-  const cityName = response.list[0].name;
-  const cityTemperature = response.list[1].main.temp;
+    console.log(response.data);
+    const wheatherInformation = document.querySelector(".weather");
+    const cityName = response.list[0].name;
+    const cityTemperature = response.list[1].main.temp;
 
-  wheatherInformation.innerHTML(`
+    wheatherInformation.innerHTML(`
     <h2>${cityName}</h2>
       <div>
         <img src="http://openweathermap.org/img/wn/10d@2x.png" alt="" />
@@ -23,16 +24,17 @@ function createHTML(response) {
 }
 
 function getLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
-  } else {
-    console.log("Geolocation is not supported by this browser.");
-  }
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        console.log("Geolocation is not supported by this browser.");
+    }
 }
 
 function showPosition(position) {
-  latitude = position.coords.latitude;
-  longitude = position.coords.longitude;
+    latitude = position.coords.latitude;
+    longitude = position.coords.longitude;
 }
 
-getLocation();
+// getLocation();
+callApiWeather();
